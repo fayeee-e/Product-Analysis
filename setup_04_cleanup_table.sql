950841ef979f02fb113a8b249a1e4978426618e3
fayeee-e <57103980+fayeee-e@users.noreply.github.com>
2022-08-31 12:02:23 +0300

    Update setup_04_cleanup_table.sql

--- setup_04_cleanup_table.sql
+++ setup_04_cleanup_table.sql
@@ -2,7 +2,7 @@
 -- Please make sure you are connected with a user with the correct privileges
 -- to run the statements
 
--- On wefarmtestdb
+-- On xyz
 DROP TABLE public.target_group;
 DROP TABLE public.target_group_user;
 DROP TABLE public.impression;
